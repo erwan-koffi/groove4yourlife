7aee8c7fd34697a91524e5b04ba2d5ee0d50c7e1
Erwan <erwan.koffi@gmail.com>
2021-11-06 02:52:55 +0100

    * better spawn

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float maxSpeed = 5f;
+
+    Rigidbody2D body;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+      body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {}
+
+    private void FixedUpdate() {
+      transform.Translate(Vector3.left * Time.deltaTime);
     }
 }
