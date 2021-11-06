fff457e0ddad9fe00a448024d56dc9a40148757a
erwan <erwan.koffi@gmail.com>
2021-11-06 21:00:58 +0100

    Darker Light

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,16 +8,19 @@ public class Weapon : MonoBehaviour
     public Transform firepoint;
     public GameObject bulletPrefab;
     public float lastShootTime = 0f;
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start() {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetButtonDown("Fire1")) {
             Shoot();
+            audioSource.pitch = (int)Random.Range(1f, 4f);
+            audioSource.Play(0);
             FindObjectOfType<GameManager>().manageMultiplier(lastShootTime, Time.fixedTime);
             lastShootTime = Time.fixedTime;
         }
